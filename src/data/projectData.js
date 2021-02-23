5c312ef987101913e7fe42ddc66f2fa8bf062ff3
Eli <eli.vercollier@gmail.com>
2021-02-23 18:02:08 +0100

    data for the project

---
+++ src/data/projectData.js
@@ -0,0 +1,38 @@
+export const projectData = [
+    {
+        id:1,
+        img: "./public/images/profil-jeanne.jpg",
+        background: "./public/images/background-jeanne.jpg",
+        name: "Jeanne",
+        phone: "06 21 00 17 18",
+        email: "jeanne.exo@gmail.com",
+        adress: "125 rue du chocolat 54000 Nancy"
+    },
+    {
+        id:2,
+        img: "./public/images/profil-eric.jpg",
+        background: "./public/images/background-eric.jpg",
+        name: "Eric",
+        phone: "06 21 00 22 23",
+        email: "eric.exo@gmail.com",
+        adress: "48 rue de la vanille 54000 Nancy"
+    },
+    {
+        id:3,
+        img: "./public/images/profil-camille.jpg",
+        background: "./public/images/background-camille.jpg",
+        name: "Camille",
+        phone: "06 21 00 18 19",
+        email: "camille.exo@gmail.com",
+        adress: "63 boulevard pistache 51100 Reims"
+    },
+    {
+        id:4,
+        img: "./public/images/profil-samuel.jpg",
+        background: "./public/images/background-samuel.jpg",
+        name: "Samuel",
+        phone: "06 21 00 20 21",
+        email: "samuel.exo@gmail.com",
+        adress: "8 place de la menthe 10000 Troyes"
+    },
+]
